cee2f9f4d3bada88e3d697a91a903ce29ac5509b
Luciana <8088524+lucianak@users.noreply.github.com>
2022-10-18 12:31:09 +0200

    Modified to call the mediawiki API running on xampp

--- src/node-backend/server.js
+++ src/node-backend/server.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const { mwn } = require('mwn');
+
+const test = new mwn({
+  apiUrl: 'http://localhost/mediawiki/api.php',
+  userAgent: 'testApiAccess'
+});
+
+const mainPage = test.read('Main Page');
+let title = ""
+mainPage.then(res => {
+  title = res.title.toString()
+})
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World, again! ' + title)
 })
 
 app.listen(port, () => {

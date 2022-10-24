2b80ecf04bd3a6b645db0d0366dbfceb35f1b2ad
quaverous2 <contact@antonioancona.it>
2022-10-24 22:47:23 +0200

    Moved modules folder to correct path + created signup module
    https://github.com/WomenPlusPlus/deploy-impact-22-wikimini-b/issues/45

--- src/angular-frontend/src/app/app.module.ts
+++ src/angular-frontend/src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomepageModule } from '../modules/homepage/homepage.module';
-import { UserpageModule } from '../modules/userpage/userpage.module';
+import { HomepageModule } from './modules/homepage/homepage.module';
+import { UserpageModule } from './modules/userpage/userpage.module';
 
 @NgModule({
   declarations: [

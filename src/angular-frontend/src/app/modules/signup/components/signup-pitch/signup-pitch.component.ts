70ff96572983f910d9df691bd28029e276e8bab1
quaverous2 <contact@antonioancona.it>
2022-10-24 23:10:16 +0200

    Creating components and basic teacher/student  signup structure
    https://github.com/WomenPlusPlus/deploy-impact-22-wikimini-b/issues/45

---
+++ src/angular-frontend/src/app/modules/signup/components/signup-pitch/signup-pitch.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-signup-pitch',
+  templateUrl: './signup-pitch.component.html',
+  styleUrls: ['./signup-pitch.component.css']
+})
+export class SignupPitchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/angular-frontend/src/app/modules/signup/signup.module.ts
+++ src/angular-frontend/src/app/modules/signup/signup.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { WrapperComponent } from './components/wrapper/wrapper.component';
+import { SignupChoiceComponent } from './components/signup-choice/signup-choice.component';
+import { SignupFormComponent } from './components/signup-form/signup-form.component';
+import { SignupSuccessComponent } from './components/signup-success/signup-success.component';
+import { SignupEmailConfirmationComponent } from './components/signup-email-confirmation/signup-email-confirmation.component';
+import { SignupPitchComponent } from './components/signup-pitch/signup-pitch.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    WrapperComponent,
+    SignupChoiceComponent,
+    SignupFormComponent,
+    SignupSuccessComponent,
+    SignupEmailConfirmationComponent,
+    SignupPitchComponent
+  ],
   imports: [
     CommonModule
   ]

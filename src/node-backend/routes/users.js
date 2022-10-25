9f184f181332ccc364f4ddd1c36ef7be28d5cf2a
Ali Karaki <ali.m.karaki@gmail.com>
2022-10-25 16:27:37 +0300

    initialize db

---
+++ src/node-backend/routes/users.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const router = express.Router();
+const pool = require("../maria-helper/database");
+// const bcrypt = require("bcrypt");
+
+/*
+to test on postman:
+url: localhost:3000/user/getUser
+body: {"id": <user id>} (setup on HeidiSQL)
+*/
+router.post("/getUser", async function (req, res) {
+  try {
+    const sqlQuery = "SELECT id, email, password, name FROM users WHERE id=?";
+    const rows = await pool.query(sqlQuery, req.body.id);
+    res.status(200).json(rows);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
+
+// router.post("/register", async function (req, res) {
+//   try {
+//     const { email, password } = req.body;
+
+//     const encryptedPassword = await bcrypt.hash(password, 10);
+
+//     const sqlQuery = "INSERT INTO user (email, password) VALUES (?,?)";
+//     const result = await pool.query(sqlQuery, [email, encryptedPassword]);
+
+//     res.status(200).json({ userId: result.insertId });
+//   } catch (error) {
+//     res.status(400).send(error.message);
+//   }
+// });
+
+// router.post("/login", async function (req, res) {
+//   try {
+//     const { id, password } = req.body;
+
+//     const sqlGetUser = "SELECT password FROM user WHERE id=?";
+//     const rows = await pool.query(sqlGetUser, id);
+//     if (rows) {
+//       const isValid = await bcrypt.compare(password, rows[0].password);
+//       res.status(200).json({ valid_password: isValid });
+//     }
+//     res.status(200).send(`User with id ${id} was not found`);
+//   } catch (error) {
+//     res.status(400).send(error.message);
+//   }
+// });
+
+module.exports = router;

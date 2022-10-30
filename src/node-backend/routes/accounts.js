7f8cc737badef39a1e643cf2f057443d801b6270
Luciana <8088524+lucianak@users.noreply.github.com>
2022-10-30 10:06:04 +0100

    Added some comments to routes

--- src/node-backend/routes/accounts.js
+++ src/node-backend/routes/accounts.js
@@ -2,13 +2,13 @@ import express from "express";
 import * as signupController from "../controller/accounts-controller.js";
 export const router = express.Router();
 
-// to test: use postman, select Body -> raw type, json, format: "username":"exampleUser, "password":"examplePassword"
+// complete path: /accounts/...
 
-// teacher sign up
+// teacher sign up: needs {username, password, email}
 router.post('/teacherSignup', signupController.doTeacherSignUp);
 router.post('/teacherEmailConfirmed', signupController.confirmTeacherAccount);
 
-// students sign up
+// students sign up:
 router.post('/studentSignup', signupController.doStudentSignUp);
 
 // students log in

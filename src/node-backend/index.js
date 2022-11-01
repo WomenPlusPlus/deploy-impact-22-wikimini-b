3881540f23bf7655464cc9a2a98623c52aede3cf
Luciana <8088524+lucianak@users.noreply.github.com>
2022-11-01 18:02:00 +0100

    Trying to solve merge conflicts (https://github.com/WomenPlusPlus/deploy-impact-22-wikimini-b/issues/77)

--- src/node-backend/index.js
+++ src/node-backend/index.js
@@ -13,7 +13,11 @@ app.use(express.urlencoded({ extended: false }));
  */
 
 app.get("/", (request, response) => {
-  response.status(200).send({success: true, message: "home"});
+  response
+    .status(200)
+    .send(
+      "Home"
+    );
 });
 
 import * as users from "./routes/users.js";
@@ -21,6 +25,34 @@ app.use("/user", users.router);
 
 import * as accounts from "./routes/accounts.js";
 app.use("/accounts", accounts.router);
+
+import * as classrooms from "./routes/classrooms.js";
+app.use("/classrooms", classrooms.router);
+
+// import mailer from "nodemailer";
+//
+// let mailTransporter = mailer.createTransport({
+//   service: 'gmail',
+//   auth: {
+//     user: 'thewikifactory@gmail.com',
+//     pass: 'rgegknltijasdrob'
+//   }
+// });
+//
+// let mailDetails = {
+//   from: 'thewikifactory@gmail.com',
+//   to: 'luciana.kolbeck@ik.me',
+//   subject: 'Test mail',
+//   text: 'Node.js testing mail for GeeksforGeeks'
+// };
+//
+// mailTransporter.sendMail(mailDetails, function(err, data) {
+//   if(err) {
+//     console.log('Error Occurs');
+//   } else {
+//     console.log('Email sent successfully');
+//   }
+// });
 
 /**Start listening */
 app.listen(port, () => {

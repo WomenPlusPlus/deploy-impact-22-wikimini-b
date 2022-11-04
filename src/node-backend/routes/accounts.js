0539ff56123a2a26e16b15887faa991fd620a9a7
Nurseda Topal <115588324+nursedaatopal@users.noreply.github.com>
2022-11-04 17:00:08 +0300

    Add files via upload
    teacher log in and sign up, student login and sign up implemented

---
+++ src/node-backend/routes/accounts.js
@@ -0,0 +1,16 @@
+import express from "express";
+import * as signupController from "../controller/accounts-controller.js";
+export const router = express.Router();
+// to test: use postman, select Body -> raw type, json, format: "username":"exampleUser, "password":"examplePassword"
+// teacher sign up
+router.post('/teacherSignup', signupController.doTeacherSignUp);
+router.post('/teacherEmailConfirmed', signupController.confirmTeacherAccount);
+
+// students sign up
+router.post('/studentSignup', signupController.doStudentSignUp);
+
+// students log in
+router.post('/studentLogin', signupController.doStudentLogin);
+
+// teacher log in
+router.post('/teacherLogin', signupController.doTeacherLogin);

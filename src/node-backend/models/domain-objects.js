84237576fabe937c588f79eba51cc2cfb841cc0e
Luciana <8088524+lucianak@users.noreply.github.com>
2022-11-05 16:38:20 +0100

    Created more domain objects (https://github.com/WomenPlusPlus/deploy-impact-22-wikimini-b/issues/137)

--- src/node-backend/models/domain-objects.js
+++ src/node-backend/models/domain-objects.js
@@ -6,6 +6,22 @@ export function Credentials(username = "", password = "", email = "") {
     this.email = email;
 }
 
+export const Status = {
+    Created: "Created",
+    Assigned: "Assigned",
+    InProgress: "In Progress",
+    NeedsHelp: "Needs Help",
+    StudentDone: "Done",
+    Grading: "Grading",
+    Finished: "Finished"
+};
+
+export const Topic = {
+    Language: "Language",
+    Media: "Media Literacy",
+    Subject: "Subject understanding"
+}
+
 //extend if necessary
 export function HwTask(title = "New Homework",
                        description = "New homework description", teacher, student,
@@ -18,4 +34,7 @@ export function HwTask(title = "New Homework",
     this.gradingCategories = gradingCategories;
     this.startDate = startDate;
     this.dueDate = dueDate;
+    this.doneDate = false;
+    this.gradedDate = false;
+    this.status = Status.Assigned;
 }

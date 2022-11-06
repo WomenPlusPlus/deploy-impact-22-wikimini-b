4e09c34ccb3fcd53d431fbc3ec4cd00405536faa
Luciana <8088524+lucianak@users.noreply.github.com>
2022-11-06 10:07:16 +0100

    Classroom notes https://github.com/WomenPlusPlus/deploy-impact-22-wikimini-b/issues/137

--- src/node-backend/routes/classrooms.js
+++ src/node-backend/routes/classrooms.js
@@ -2,6 +2,15 @@ import express from "express";
 import * as signupController from "../controller/classroom-controller.js";
 export const router = express.Router();
 
+
+// create classroom with passing teacher name and classname, return classId
+
+// (request class list upload, pass file?, return list of student names (and maybe emails))
+
+// save list of students?
+
+// send personal codes via email
+
+// download personal code sheet (return file?)
+
 //
-// router.post('/teacherSignup', signupController.doTeacherSignUp);
-// router.post('/teacherEmailConfirmed', signupController.confirmTeacherAccount);

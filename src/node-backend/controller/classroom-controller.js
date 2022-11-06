7963393bb1a479851283a808652fbdc615a811b4
Luciana <8088524+lucianak@users.noreply.github.com>
2022-11-06 18:36:49 +0100

    WIP: started to implement saving of students registered by teacher https://github.com/WomenPlusPlus/deploy-impact-22-wikimini-b/issues/124

---
+++ src/node-backend/controller/classroom-controller.js
@@ -0,0 +1,7 @@
+
+
+export async function saveStudentsToRegister(studentNamesEmails, classId) {
+    studentNamesEmails.forEach(student => {
+        const joinCode =
+    });
+}
--- src/node-backend/routes/classrooms.js
+++ src/node-backend/routes/classrooms.js
@@ -1,5 +1,5 @@
 import express from "express";
-
+import * as classroomController from "../controller/classroom-controller.js";
 export const router = express.Router();
 
 
@@ -8,6 +8,16 @@ export const router = express.Router();
 // (request class list upload, pass file?, return list of student names (and maybe emails))
 
 // save list of students -> a list of student names and mail addresses and a class ID
+// studentNamesEmails [{name: name, email: email}, {name: name, email: email}]
+router.post('/saveStudents', async function (req, res) {
+    try {
+        const {studentNamesEmails, classId} = req.body;
+        const result = await classroomController.saveStudentsToRegister(studentNamesEmails, classId);
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
+});
 
 // send personal codes via email, pass classId
 

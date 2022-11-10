a006ecff7d44aa5897a49baa7521b8064a8258f2
Ali Karaki <ali.m.karaki@gmail.com>
2022-11-10 13:48:06 +0200

    share student sign up codes

--- src/node-backend/adapters/database-adapter.js
+++ src/node-backend/adapters/database-adapter.js
@@ -1,14 +1,24 @@
-import {pool} from "./database-connection.js";
+import { pool } from "./database-connection.js";
 
 export async function addStudentCodes(classId, studentInfos) {
   try {
-    let insertStudentCodeQuery = "INSERT INTO `classroomcodes`(`classID`,`studentJoinCode`,`studentFullName`,`parentEmail`) VALUES ";
+    let insertStudentCodeQuery =
+      "INSERT INTO `classroomcodes`(`classID`,`studentJoinCode`,`studentFullName`,`parentEmail`) VALUES ";
     let values = "";
-    studentInfos.forEach(student => {
-      let row = "('" + classId + "', '" + student.joinCode + "', '" + student.fullName + "', '" + student.email + "'), ";
+    studentInfos.forEach((student) => {
+      let row =
+        "('" +
+        classId +
+        "', '" +
+        student.joinCode +
+        "', '" +
+        student.fullName +
+        "', '" +
+        student.email +
+        "'), ";
       values += row;
     });
-    values  = values.slice(0, -2); // remove last comma
+    values = values.slice(0, -2); // remove last comma
     insertStudentCodeQuery += values;
     return pool.query(insertStudentCodeQuery);
   } catch (error) {
@@ -23,11 +33,12 @@ export async function verifyCode(studentCode) {
     throw Error("Student code has to be 5 characters long");
   }
   try {
-    const checkCodeQuery = "SELECT teachers.username, classrooms.id, classroomCodes.studentFullName, classroomCodes.used FROM teachers, classrooms, classroomCodes " +
-        "WHERE teachers.username=classroomCodes.teacherUsername and classrooms.id=classroomCodes.classID and classroomCodes.studentJoinCode=?";
+    const checkCodeQuery =
+      "SELECT teachers.username, classrooms.id, classroomCodes.studentFullName, classroomCodes.used FROM teachers, classrooms, classroomCodes " +
+      "WHERE teachers.username=classroomCodes.teacherUsername and classrooms.id=classroomCodes.classID and classroomCodes.studentJoinCode=?";
     const queryResult = await pool.query(checkCodeQuery, studentCode);
-    const valid = (queryResult.length === 1) && (queryResult[0]["used"] === false);
-    return {"valid": valid, "result": queryResult};
+    const valid = queryResult.length === 1 && queryResult[0]["used"] === false;
+    return { valid: valid, result: queryResult };
   } catch (error) {
     console.error("Error while checking student code: " + error);
     throw error;
@@ -36,37 +47,48 @@ export async function verifyCode(studentCode) {
 
 export async function getTeacherEmailForClass(classId) {
   try {
-    const teacherEmailQuery = "SELECT teachers.email FROM teachers, classrooms WHERE teachers.username=classrooms.teacherUsername AND classrooms.id=?";
+    const teacherEmailQuery =
+      "SELECT teachers.email FROM teachers, classrooms WHERE teachers.username=classrooms.teacherUsername AND classrooms.id=?";
     return await pool.query(teacherEmailQuery, classId);
   } catch (error) {
-    console.error("Error while trying to retrieve teachers email for the given class: " + error);
+    console.error(
+      "Error while trying to retrieve teachers email for the given class: " +
+        error
+    );
     throw error;
   }
 }
 
 export async function markCodeAsUsed(code) {
   try {
-    const updateJoinCodeAsUsed = "UPDATE `classroomcodes` SET `codeUsed`=1 WHERE `studentJoinCode`=?";
+    const updateJoinCodeAsUsed =
+      "UPDATE `classroomcodes` SET `codeUsed`=1 WHERE `studentJoinCode`=?";
     return pool.query(updateJoinCodeAsUsed, code);
   } catch (error) {
-    console.error("Error while trying to mark student signup code as used: " + error);
+    console.error(
+      "Error while trying to mark student signup code as used: " + error
+    );
     throw error;
   }
 }
 
 export async function addStudent(username, fullName) {
   try {
-    const insertStudentQuery = "INSERT INTO `students`(`username`, `fullName`) VALUES (?,?)";
+    const insertStudentQuery =
+      "INSERT INTO `students`(`username`, `fullName`) VALUES (?,?)";
     return pool.query(insertStudentQuery, [username, fullName]);
   } catch (error) {
-    console.error("Error while trying to register student in database: " + error);
+    console.error(
+      "Error while trying to register student in database: " + error
+    );
     throw error;
   }
 }
 
 export async function enrollStudentInClass(username, classId) {
   try {
-    const enrollStudentQuery = "INSERT INTO `classesEnrolled`(`classId`, `username`) VALUES (?,?)";
+    const enrollStudentQuery =
+      "INSERT INTO `classesEnrolled`(`classId`, `username`) VALUES (?,?)";
     return pool.query(enrollStudentQuery, [classId, username]);
   } catch (error) {
     console.error("Error while trying to enroll student in class: " + error);
@@ -76,37 +98,47 @@ export async function enrollStudentInClass(username, classId) {
 
 export async function registerTeacher(username, email) {
   try {
-    const insertTeacherQuery = "INSERT INTO `teachers`(`username`, `email`) VALUES (?,?)";
+    const insertTeacherQuery =
+      "INSERT INTO `teachers`(`username`, `email`) VALUES (?,?)";
     return pool.query(insertTeacherQuery, [username, email]);
   } catch (error) {
-    console.error("Error while trying to register teacher in database: " + error);
+    console.error(
+      "Error while trying to register teacher in database: " + error
+    );
     throw error;
   }
 }
 
 export async function registerTeacherAuthCode(username, authCode) {
   try {
-    const insertAuthCodeQuery = "INSERT INTO `teacherAuth`(`username`, `authCode`) VALUES (?,?)";
+    const insertAuthCodeQuery =
+      "INSERT INTO `teacherAuth`(`username`, `authCode`) VALUES (?,?)";
     return pool.query(insertAuthCodeQuery, [username, authCode]);
   } catch (error) {
-    console.error("Error while trying to store teacher authentication code: " + error);
+    console.error(
+      "Error while trying to store teacher authentication code: " + error
+    );
     throw error;
   }
 }
 
 export async function registerTeacherAsVerified(username) {
   try {
-    const updateTeacherQuery = "UPDATE `teachers` SET `isVerified`=1 WHERE `username`=?";
+    const updateTeacherQuery =
+      "UPDATE `teachers` SET `isVerified`=1 WHERE `username`=?";
     return pool.query(updateTeacherQuery, username);
   } catch (error) {
-    console.error("Error while trying to register teacher as verified in db: " + error);
+    console.error(
+      "Error while trying to register teacher as verified in db: " + error
+    );
     throw error;
   }
 }
 
 export async function getTeacherAuthCode(username) {
   try {
-    const teacherAuthQuery = "SELECT `authCode` FROM `teacherAuth` WHERE `teacherAuth`.`username`=?";
+    const teacherAuthQuery =
+      "SELECT `authCode` FROM `teacherAuth` WHERE `teacherAuth`.`username`=?";
     const result = await pool.query(teacherAuthQuery, username);
     if (result.length === 1) {
       return result[0]["authCode"];
@@ -114,22 +146,40 @@ export async function getTeacherAuthCode(username) {
       throw Error("Error while trying to verify authentication code");
     }
   } catch (error) {
-    console.error("Error while trying to retrieve authentication code: " + error);
+    console.error(
+      "Error while trying to retrieve authentication code: " + error
+    );
     throw error;
   }
 }
 
 export async function getTeacherInfo(user) {
   try {
-    const teacherUsernameQuery = "SELECT `username`, `email`, `isVerified` FROM `teachers` WHERE `teachers`.`username`=? OR `teachers`.`email`=?";
+    const teacherUsernameQuery =
+      "SELECT `username`, `email`, `isVerified` FROM `teachers` WHERE `teachers`.`username`=? OR `teachers`.`email`=?";
     const result = await pool.query(teacherUsernameQuery, [user, user]);
     if (result.length === 1) {
       return result[0];
     } else {
-      throw Error("Error while trying to find teacher with the provided login credentials");
+      throw Error(
+        "Error while trying to find teacher with the provided login credentials"
+      );
     }
   } catch (error) {
     console.error("Error while trying to retrieve user: " + error);
+    throw error;
+  }
+}
+
+export async function getStudentCodesForTeacher(username) {
+  try {
+    const getQuery =
+      "SELECT `studentJoinCode`, `parentEmail` FROM classroomcodes WHERE classId = (SELECT `id` FROM classrooms WHERE teacherUsername =?);";
+    return pool.query(getQuery, username);
+  } catch (error) {
+    console.error(
+      "Error while trying to store teacher authentication code: " + error
+    );
     throw error;
   }
 }
--- src/node-backend/controller/accounts-controller.js
+++ src/node-backend/controller/accounts-controller.js
@@ -1,7 +1,7 @@
 import * as wikiAdapter from "../adapters/wiki-adapter.js";
 import * as dbAdapter from "../adapters/database-adapter.js";
 import * as emailAdapter from "../adapters/email-adapter.js";
-import {Credentials} from "../models/domain-objects.js";
+import { Credentials } from "../models/domain-objects.js";
 
 const returnUrl = "http://localhost/mediawiki";
 const teacherUserGroup = "teacher";
@@ -13,172 +13,237 @@ const createAccountAction = "createaccount";
 const loginAction = "clientlogin";
 
 export const doTeacherSignUp = async (req, res) => {
-    try {
-        const {username, password, email} = req.body;
-        const credentials = new Credentials(username, password, email);
-        await dbAdapter.registerTeacher(username, email);
-        const teacherSignUpResult = await createNewAccount(credentials);
-        const emailSent = await sendTeacherConfirmationMail(username, email);
-        if (teacherSignUpResult[createAccountAction]["status"] === "PASS" && emailSent) {
-            res.status(200).json({success: true, res: teacherSignUpResult});
-        } else {
-            res.status(405).json({success: false, res: teacherSignUpResult[createAccountAction]});
-        }
-    } catch (error) {
-        res.status(405).json({ message: error.message });
+  try {
+    const { username, password, email } = req.body;
+    const credentials = new Credentials(username, password, email);
+    await dbAdapter.registerTeacher(username, email);
+    const teacherSignUpResult = await createNewAccount(credentials);
+    const emailSent = await sendTeacherConfirmationMail(username, email);
+    if (
+      teacherSignUpResult[createAccountAction]["status"] === "PASS" &&
+      emailSent
+    ) {
+      res.status(200).json({ success: true, res: teacherSignUpResult });
+    } else {
+      res.status(405).json({
+        success: false,
+        res: teacherSignUpResult[createAccountAction],
+      });
     }
-
-}
+  } catch (error) {
+    res.status(405).json({ message: error.message });
+  }
+};
 export const confirmTeacherAccount = async (req, res) => {
-    try {
-        const {username, authCode} = req.body;
-        const savedCode = await dbAdapter.getTeacherAuthCode(username);
-        if (savedCode === authCode) {
-            await dbAdapter.registerTeacherAsVerified(username);
-            await registerUserAsTeacher(username);
-            res.status(200).json("VALID");
-        } else {
-            throw Error("Email for user " + username + "could not be confirmed");
-        }
-    } catch (error) {
-        res.status(406).json({message: error.message });
+  try {
+    const { username, authCode } = req.body;
+    const savedCode = await dbAdapter.getTeacherAuthCode(username);
+    if (savedCode === authCode) {
+      await dbAdapter.registerTeacherAsVerified(username);
+      await registerUserAsTeacher(username);
+      res.status(200).json("VALID");
+    } else {
+      throw Error("Email for user " + username + "could not be confirmed");
     }
-}
+  } catch (error) {
+    res.status(406).json({ message: error.message });
+  }
+};
 export const doStudentSignUp = async (req, res) => {
-    try {
-        const {username, password, code} = req.body;
-        const studentInfos = await dbAdapter.verifyCode(code);
-        // if valid, register student to classroom and teacher
-        if (studentInfos.valid) {
-            // invalidate code
-            await dbAdapter.markCodeAsUsed(code);
-            // add student to our db
-            await dbAdapter.addStudent(username, studentInfos.result["assignedName"]);
-            // register student to classroom
-            await dbAdapter.enrollStudentInClass(username, studentInfos.result["id"]);
-        } else {
-            throw Error("The signup code is not valid");
-        }
-        // get teacher email
-        const email = dbAdapter.getTeacherEmailForClass(studentInfos.result["id"]);
-        const credentials = new Credentials(username, password, email);
-        const studentSignUpResult = await createNewAccount(credentials);
-        res.status(200).json(studentSignUpResult);
-    } catch (error) {
-        res.status(407).json({ message: error.message });
+  try {
+    const { username, password, code } = req.body;
+    const studentInfos = await dbAdapter.verifyCode(code);
+    // if valid, register student to classroom and teacher
+    if (studentInfos.valid) {
+      // invalidate code
+      await dbAdapter.markCodeAsUsed(code);
+      // add student to our db
+      await dbAdapter.addStudent(username, studentInfos.result["assignedName"]);
+      // register student to classroom
+      await dbAdapter.enrollStudentInClass(username, studentInfos.result["id"]);
+    } else {
+      throw Error("The signup code is not valid");
     }
-}
+    // get teacher email
+    const email = dbAdapter.getTeacherEmailForClass(studentInfos.result["id"]);
+    const credentials = new Credentials(username, password, email);
+    const studentSignUpResult = await createNewAccount(credentials);
+    res.status(200).json(studentSignUpResult);
+  } catch (error) {
+    res.status(407).json({ message: error.message });
+  }
+};
 
 export const doStudentLogin = async (req, res) => {
-    try {
-        const {username, password} = req.body;
-        const credentials = new Credentials(username, password);
-        const loginResult = await login(credentials);
-        res.status(200).json(loginResult);
-    } catch (error) {
-        res.status(408).json({ message: error.message });
-    }
-}
+  try {
+    const { username, password } = req.body;
+    const credentials = new Credentials(username, password);
+    const loginResult = await login(credentials);
+    res.status(200).json(loginResult);
+  } catch (error) {
+    res.status(408).json({ message: error.message });
+  }
+};
 
 export const doTeacherLogin = async (req, res) => {
-    try {
-        const {user, password} = req.body;
-        const userInfo = await dbAdapter.getTeacherInfo(user);
-        if (userInfo['isVerified'] === 0) {
-            throw Error("The user did not verify their email address, not proceeding with login");
-        }
-        const credentials = new Credentials(userInfo['username'], password);
-        const loginResult = await login(credentials);
-        res.status(200).json(loginResult);
-    } catch (error) {
-        res.status(408).json({ message: error.message });
+  try {
+    const { user, password } = req.body;
+    const userInfo = await dbAdapter.getTeacherInfo(user);
+    if (userInfo["isVerified"] === 0) {
+      throw Error(
+        "The user did not verify their email address, not proceeding with login"
+      );
     }
-}
+    const credentials = new Credentials(userInfo["username"], password);
+    const loginResult = await login(credentials);
+    res.status(200).json(loginResult);
+  } catch (error) {
+    res.status(408).json({ message: error.message });
+  }
+};
 
 async function login(credentials = new Credentials()) {
-    const token = await wikiAdapter.getTokenOfType("login");
-    const loginResult = await wikiAdapter.request({
-        action: loginAction,
-        username: credentials.username,
-        password: credentials.password,
-        logintoken: token,
-        loginreturnurl: returnUrl,
-    }).catch((e) => {
-        if (e.code === noSuchUserCode) {
-            throw Error("No such user exists, can't login");
-        } else {
-            throw Error("Error while trying to log in: " + e);
-        }
+  const token = await wikiAdapter.getTokenOfType("login");
+  const loginResult = await wikiAdapter
+    .request({
+      action: loginAction,
+      username: credentials.username,
+      password: credentials.password,
+      logintoken: token,
+      loginreturnurl: returnUrl,
+    })
+    .catch((e) => {
+      if (e.code === noSuchUserCode) {
+        throw Error("No such user exists, can't login");
+      } else {
+        throw Error("Error while trying to log in: " + e);
+      }
     });
-    loginResult[loginAction]["token"] = await wikiAdapter.getEditToken();
-    return loginResult;
+  loginResult[loginAction]["token"] = await wikiAdapter.getEditToken();
+  return loginResult;
 }
 
 async function createNewAccount(credentials = new Credentials()) {
-    const username = credentials.username;
-    const password = credentials.password;
-    const email = credentials.email;
+  const username = credentials.username;
+  const password = credentials.password;
+  const email = credentials.email;
 
-    await wikiAdapter.request({
-        action: validatePasswordAction,
-        password: password,
-        user: username,
-        email: email,
-    }).catch(e => {
-        if (e.code === userExistsCode) {
-            throw Error("User does already exist, can't register");
-        } else {
-            throw Error("Error while validating signup data" + e);
-        }
+  await wikiAdapter
+    .request({
+      action: validatePasswordAction,
+      password: password,
+      user: username,
+      email: email,
+    })
+    .catch((e) => {
+      if (e.code === userExistsCode) {
+        throw Error("User does already exist, can't register");
+      } else {
+        throw Error("Error while validating signup data" + e);
+      }
     });
 
-    const token = await wikiAdapter.getAccountCreationToken();
-    return wikiAdapter.request({
-        action: createAccountAction,
-        username: username,
-        password: password,
-        retype: password,
-        email: email,
-        createtoken: token,
-        createreturnurl: returnUrl,
-        reason: "New account creation through Wikimini"
-    }).catch((e) => {
-        if (e.code === userExistsCode) {
-            throw Error("User does already exist, can't register");
-        } else {
-            throw Error("Error in creating new user: " + e);
-        }
+  const token = await wikiAdapter.getAccountCreationToken();
+  return wikiAdapter
+    .request({
+      action: createAccountAction,
+      username: username,
+      password: password,
+      retype: password,
+      email: email,
+      createtoken: token,
+      createreturnurl: returnUrl,
+      reason: "New account creation through Wikimini",
+    })
+    .catch((e) => {
+      if (e.code === userExistsCode) {
+        throw Error("User does already exist, can't register");
+      } else {
+        throw Error("Error in creating new user: " + e);
+      }
     });
 }
 
 export async function sendTeacherConfirmationMail(username, email) {
-    const authCode = Math.floor(Math.random() * 100000);
-    const authCodeRegistered = await dbAdapter.registerTeacherAuthCode(username, authCode);
-    if (!authCodeRegistered) {
-        throw Error("Problem while saving auth code to database");
-    }
-    const subject = "Confirm your Wikimini account";
-    const text = "Hello " + username + ", \nPlease confirm the account you just created on Wikimini by clicking on the following link: \n\n"
-        + process.env.WEB_URL + "&username=" + username + "&authCode=" + authCode;
-    return emailAdapter.sendEmail(email, subject, text);
+  const authCode = Math.floor(Math.random() * 100000);
+  const authCodeRegistered = await dbAdapter.registerTeacherAuthCode(
+    username,
+    authCode
+  );
+  if (!authCodeRegistered) {
+    throw Error("Problem while saving auth code to database");
+  }
+  const subject = "Confirm your Wikimini account";
+  const text =
+    "Hello " +
+    username +
+    ", \nPlease confirm the account you just created on Wikimini by clicking on the following link: \n\n" +
+    process.env.WEB_URL +
+    "&username=" +
+    username +
+    "&authCode=" +
+    authCode;
+  return emailAdapter.sendEmail(email, subject, text);
 }
 
 async function registerUserAsTeacher(username) {
-    // login with a bot account that has the right to change usergroups (safety issue?),
-    const token = await wikiAdapter.getLocalEditToken();
-    await wikiAdapter.request({
-        action: userRightsAction,
-        user: username,
-        add: teacherUserGroup,
-        reason: "New teacher's account creation",
-        token: token
-    }).then(response => {
-        if (response.userrights.added[0] === teacherUserGroup) {
-            return true;
-        } else {
-            throw Error("Error while trying to register the account as a teacher's account: " + response.userrights); // should have a nicer format
-        }
-    }).catch(e => {
-        throw Error("Error while trying to register the account as a teacher's account: " + e);
+  // login with a bot account that has the right to change usergroups (safety issue?),
+  const token = await wikiAdapter.getLocalEditToken();
+  await wikiAdapter
+    .request({
+      action: userRightsAction,
+      user: username,
+      add: teacherUserGroup,
+      reason: "New teacher's account creation",
+      token: token,
+    })
+    .then((response) => {
+      if (response.userrights.added[0] === teacherUserGroup) {
+        return true;
+      } else {
+        throw Error(
+          "Error while trying to register the account as a teacher's account: " +
+            response.userrights
+        ); // should have a nicer format
+      }
+    })
+    .catch((e) => {
+      throw Error(
+        "Error while trying to register the account as a teacher's account: " +
+          e
+      );
     });
 }
+
+export async function sendStudentCodesForParents(username, emails) {
+  const list = await dbAdapter.getStudentCodesForTeacher(username);
+  const subject = "Account signup code";
+  for (let i = 0; i < list.length; i++) {
+    if (emails.includes(list[i].parentEmail)) {
+      await emailAdapter.sendEmail(
+        list[i].parentEmail,
+        subject,
+        generateMessage(list[i].studentJoinCode)
+      );
+    }
+  }
+  return true;
+}
+
+export const shareCodesWithParents = async (req, res) => {
+  try {
+    const { username, emails } = req.body;
+    const emailsSent = await sendStudentCodesForParents(username, emails);
+    res.status(200).json({ success: true, res: "emails Sent" });
+  } catch (error) {
+    res.status(405).json({ message: error.message });
+  }
+};
+
+const generateMessage = (code) => {
+  return (
+    "Please enter the code " +
+    code +
+    " while signing up, by clicking on the followinlink: \n\n + http://localhost:4200/signup"
+  );
+};
